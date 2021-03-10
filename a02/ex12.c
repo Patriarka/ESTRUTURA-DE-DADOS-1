54b4cbda41e3001d2aaba91659158ed57b5c054b
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2021-03-10 20:15:42 -0300

    Update ex12.c

--- a02/ex12.c
+++ a02/ex12.c
@@ -7,25 +7,18 @@ int Contar_String(char* str){
   return i;
 }
 
-char* Copia_Str(char* str1, char* str2){
-
+void Copia_String(char* str1, char* str2){
   int tam = Contar_String(str1);
-
-  int i;
-  for(i = 0; i < tam; i++)
+  for(int i = 0; i <= tam; i++)
     str2[i] = str1[i];
-  str2[i] = '\0';
-
-  return 0;
 }
 
 int main(){
+  
+  char str1[] = {'B','u','l','l','s','\0'};
+  char str2[] = {'W','a','r','r','i','o','r','s','\0'};
 
-  char str1[] = {'B','U','L','L','S','\0'};
-  char str2[] = {'W','A','R','R','I','O','R','S','\0'};
-
-  printf("%s\n", str2);
-  Copia_Str(str1, str2);
+  Copia_String(str1, str2);
   printf("%s\n", str2);
 
   return 0;
