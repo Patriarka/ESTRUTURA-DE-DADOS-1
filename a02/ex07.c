3a5887f7013b070a407c4a5f248d5c6f1be82896
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2021-03-10 21:27:31 -0300

    Update ex07.c

--- a02/ex07.c
+++ a02/ex07.c
@@ -28,6 +28,7 @@ int main(){
 
   int maior;
   int v[] = {1,2,7,4,5};
+  int v2[] = {1,-2,-7,4,5};
 
   printf("Maior: %d\n",Maior_Elemento1(v,-5));
   printf("Maior: %d\n",Maior_Elemento1(v,5));
@@ -35,6 +36,10 @@ int main(){
   Maior_Elemento2(v,5,&maior);
   printf("Maior: %d\n", maior);
   Maior_Elemento2(v,-5,&maior);
+  printf("Maior: %d\n", maior);
+
+  printf("Maior: %d\n",Maior_Elemento1(v2,5));
+  Maior_Elemento2(v2,5,&maior);
   printf("Maior: %d\n", maior);
 
   return 0;
