e146187dca3b9f5790336af813b96a3f0a85a5a2
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2021-03-10 21:48:23 -0300

    Update ex08.c

--- a02/ex08.c
+++ a02/ex08.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* Intercalar_Vetores1(int* v1, int tam1, int* v2, int tam2){
+int* Intercalar_Vetores1(int* v1, int tam1, int* v2, int tam2, int* vResultante){
 
   int tamResultante = tam1+tam2;
-  int* vResultante = (int*) calloc(tamResultante, sizeof(int));
   
   int i, j, k;
   for(i = 0, j = 0, k = 0; k < tamResultante; k++){
@@ -17,17 +16,16 @@ int* Intercalar_Vetores1(int* v1, int tam1, int* v2, int tam2){
   return vResultante;
 }
 
-void Intercalar_Vetores2(int* v1, int tam1, int* v2, int tam2, int** vResultante){
+void Intercalar_Vetores2(int* v1, int tam1, int* v2, int tam2, int* vResultante){
 
   int tamResultante = tam1+tam2;
-  *vResultante = (int*) calloc(tamResultante, sizeof(int));
   
   int i, j, k;
   for(i = 0, j = 0, k = 0; k < tamResultante; k++){
     if(v2[j] <= v1[i] && j < tam2)
-      (*vResultante)[k] = v2[j++];
+      vResultante[k] = v2[j++];
     else
-      (*vResultante)[k] = v1[i++];
+      vResultante[k] = v1[i++];
   }
 }
 
@@ -44,19 +42,34 @@ void Vet_Imprimir(int* v, int qtdeElementos){
 
 int main(){
 
+  int vResultante[9]; 
   int* vResultante2;
   int v1[] = {6,7,8,9,10};
   int v2[] = {6,7,8,9};
 
-  int* vResultante = Intercalar_Vetores1(v1, 5, v2, 4);
+  int* resultante = Intercalar_Vetores1(v1, 5, v2, 4, vResultante);
+  Vet_Imprimir(resultante,9);
 
-  Intercalar_Vetores2(v1, 5, v2, 4, &vResultante2);
+  Intercalar_Vetores2(v1, 5, v2, 4, vResultante);
+  Vet_Imprimir(vResultante,9);
 
-  Vet_Imprimir(vResultante,5+4);
-  Vet_Imprimir(vResultante2,5+4);
+  int v3[] = {1,2,3,4,5};
+  int v4[] = {1,2,3,4};
 
-  free(vResultante);
-  free(vResultante2);
+  resultante = Intercalar_Vetores1(v3, 5, v4, 4, vResultante);
+  Vet_Imprimir(resultante,9);
+
+  Intercalar_Vetores2(v3, 5, v4, 4, vResultante);
+  Vet_Imprimir(vResultante,9);
+
+  int v5[] = {-5,-1,2,3,4};
+  int v6[] = {-3,1,2,4};
+
+  resultante = Intercalar_Vetores1(v5, 5, v6, 4, vResultante);
+  Vet_Imprimir(resultante,9);
+
+  Intercalar_Vetores2(v5, 5, v6, 4, vResultante);
+  Vet_Imprimir(vResultante,9); 
 
   return 0;
 }
